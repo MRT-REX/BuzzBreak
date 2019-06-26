a9eda70b25fb2a618f9acaf006022e97a4c88ad5
MRT-REX <48020975+MRT-REX@users.noreply.github.com>
2019-06-26 17:07:26 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,24 @@
+<?php
+
+$request_body= '{
+	"device_model":"A16C3H",
+	"app_version_code":20,
+	"device_type":"android",
+	"facebook_user_id":"132609464492916",
+	"device_id":"44f5e10f-3a15-4d8b-9fe1-0443f28d11aa",
+	"facebook_access_token":"EAAQ229COIU4BAAOssjCTPY0AFqKir8R2jwIL0y1sKNuQCSZCx9JZAKAXP1wJ7QrDejm8tA66FUghq7j09ZAiicfQWhBvXiAOtnd273M7POOzxipyRyzqi7OE2dIVSBodkSRieC72oqK058k7ZBgelH84K7zXT0UvAcEvyGwAM1PrjYndbB8moAwCo3TkIshreppBPPBeZBSi27o1EzXy2ZAmFZAOJgCkroZD",
+	"visitor_id":117135
+}';
+
+
+
+//header
+$Voyager_API_Key="p5DqGsYcOFucDadCvfWyjgrbk3Bs1RE1";
+$Buzzbreak_API_Key="p5DqGsYcOFucDadCvfWyjgrbk3Bs1RE1";
+$Buzzbreak_Device_ID="44f5e10f-3a15-4d8b-9fe1-0443f28d11aa";
+$Account_ID="117143";
+
+ 
+
+
+?>
